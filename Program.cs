de3967652d607e0610cb7ab932407efa8415bca7
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-12-15 23:28:32 +0530

    client:fix: load host from appsettings.json

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿using TcpStatusServer;
+﻿using Microsoft.Extensions.Configuration;
+using TcpStatusServer;
 
 class Program
 {
     static async Task Main(string[] args)
     {
-        var server = new StatusServer(5000);
+        // laod configurations
+        var config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+
+        // read connection details
+        var portStr = config["Server:Port"] ?? throw new InvalidOperationException("Server:Port not configured");
+        var port = int.Parse(portStr);
+
+
+        var server = new StatusServer(port);
         await server.StartASync();
     }
 }
