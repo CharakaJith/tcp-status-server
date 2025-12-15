eed62fff42ebd893095d5bcda0dc21fe291edbc7
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-12-15 16:09:48 +0530

    server: handle async method implemented in clientconnections

--- Server/ClientConnection.cs
+++ Server/ClientConnection.cs
@@ -15,5 +15,47 @@ namespace TcpStatusServer.Server
             this._stream = client.GetStream();
             this._onDisconnect = onDisconnect;
         }
+
+        public async Task HandleAsync()
+        {
+            var buffer = new byte[1024];
+
+            try
+            {
+                while(this._client.Connected)
+                {
+                    int bytesRead = await this._stream.ReadAsync(buffer, 0, buffer.Length);
+
+                    if (bytesRead == 0)
+                        break;
+
+                    var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    HandleMessage(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Client error: {ex.Message}");
+            }
+            finally
+            {
+                this._client.Close();
+                this._onDisconnect(this);
+            }
+        }
+
+        private void HandleMessage(string message)
+        {
+            var output = message switch
+            {
+                var msg when msg.StartsWith("ACK") => "ACK received from client",
+                var msg when msg.StartsWith("STATUS_REPLY") => $"Status received: {message}",
+                var msg when msg.StartsWith("BUSY") => "Client is busy",
+                var msg when msg.StartsWith("ERROR") => $"Client error: {message}",
+                _ => $"Unknown message: {message}"
+            };
+
+            Console.WriteLine(output);
+        }
     }
 }
